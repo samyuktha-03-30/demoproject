0811d8d3b433427d3ac6dbcd1aaa30bd09baa74d
samyuktha-03-30 <samyukthas595@gmail.com>
2025-06-20 12:24:17 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,20 @@
+/*var i=0;
+while(i<5);
+{
+    console.log(i);
+    i++;
+}*/
+
+for (var i = 5;i > 5;i--)
+{
+    document.write(i);
+}
+ 
+for(var i=0;i<5;i++,document.write(i)); /*another way of writing for loop */
+
+vari=0;
+while(i<5)
+{
+    document.write(i);
+    i++
+}
